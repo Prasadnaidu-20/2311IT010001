1bdb2bb2f6aa1cb510eeff0cf8b3bacca3b5d428
Devi Naga Prasad <2311it010001@mallareddyuniversity.ac.in>
2026-07-01 18:00:18 +0530

    schedule route

--- vehicle-scheduler-be/src/routes/sheduleRoutes.js
+++ vehicle-scheduler-be/src/routes/sheduleRoutes.js
@@ -3,7 +3,6 @@ import { generateSchedule } from "../controllers/sheduleController.js";
 
 const router = express.Router();
 
-// Generate optimal maintenance schedule
 router.get("/schedule", generateSchedule);
 
 export default router;
